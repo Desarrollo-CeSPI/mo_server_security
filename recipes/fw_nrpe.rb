e5f4f5351c5a4385dd3dbb3e80656463ac3fdd49
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2015-05-04 15:21:54 -0300

    Recipe fw_monitoring replace fw_nrpe and viceversa.
    Logic in fw_monitoring and fw_nrpe was inverted.

--- recipes/fw_nrpe.rb
+++ recipes/fw_nrpe.rb
@@ -5,8 +5,10 @@ search(:node, "role:#{node['nrpe']['server_role']}") do |host|
   monitoring_hosts << host['ipaddress']
 end
 
+node.set[:mo_server_security][:firewall][:fw_nrpe][:allowed_networks][:networks] = monitoring_hosts
+
 simple_iptables_rule "monitoring" do
   direction "INPUT"
-  rule all_from(monitoring_hosts)
+  rule rule_for("nrpe")
   jump "ACCEPT"
 end
