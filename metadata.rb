d1f5521d3e0dc1e9f3af9d28cbdbc64e1b214b2b
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-06-24 16:55:08 -0300

    add syslog recipe

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chrodriguez@gmail.com leandro.ditommaso@mikroways.net'
 license          'MIT'
 description      'Installs/Configures mo_server_security'
 long_description 'Installs/Configures mo_server_security'
-version          '1.1.4'
+version          '1.1.5'
 
 depends 'chef-sugar',       '~> 2.5.0'
 depends 'mo_server_base',   '>= 0.0.0'
