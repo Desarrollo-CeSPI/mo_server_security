d6774005e042d73874957e9c0bc26f80aeec2976
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2015-06-25 15:23:21 -0300

    Accept more than one superadmin group.

--- recipes/restricted_su.rb
+++ recipes/restricted_su.rb
@@ -1,5 +1,7 @@
-file "/bin/su" do
-  owner 'root'
-  group node[:mo_server_base][:authorization][:superadmin_group]
-  mode '4750'
+Array(node[:mo_server_base][:authorization][:superadmin_group]).each do |g|
+  file "/bin/su" do
+    owner 'root'
+    group g
+    mode '4750'
+  end
 end
